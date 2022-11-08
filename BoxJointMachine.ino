ff6507c969f4fbe20c7d242acb9e71c4bdf74b6a
Dan Higham <dan@high.am>
2022-11-08 11:14:14 -0800

    Encoder button impl.

--- BoxJointMachine.ino
+++ BoxJointMachine.ino
@@ -1,31 +1,31 @@
-
 #include <EncoderButton.h>
-EncoderButton eb1(51, 49, 53);
+// EncoderButton eb1(51, 49, 53);
+EncoderButton eb1(50, 48, 52);
 
-#include <UTFTGLUE.h>              //use GLUE class and constructor
-UTFTGLUE myGLCD(0,A2,A1,A3,A4,A0); //all dummy args
+#include <Encoder.h>
 
-/**
- * A function to handle the 'clicked' event
- * Can be called anything but requires EncoderButton& 
- * as its only parameter.
- * I tend to prefix with 'on' and suffix with the 
- * event type.
- */
-void onEb1Clicked(EncoderButton& eb) {
-  Serial.print("eb1 clickCount: ");
-  Serial.println(eb.clickCount());
-}
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
 
-/**
- * A function to handle the 'encoder' event
- */
-void onEb1Encoder(EncoderButton& eb) {
-  Serial.print("eb1 incremented by: ");
-  Serial.println(eb.increment());
-  Serial.print("eb1 position is: ");
-  Serial.println(eb.position());
-}
+// Assign human-readable names to some common 16-bit color values:
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+#define GRAY    0x8410
+
+#define SCREEN_MARGIN 32
+#define TEXT_SCREEN_MARGIN 40
+#define VERTICAL_TEXT_SPACING 10
+
+#define R_CLK_PIN  48
+#define R_DATA_PIN 50
+
+uint16_t version = MCUFRIEND_KBV_H_;
 
 void testFunction() {
   Serial.println("foo");
@@ -43,65 +43,144 @@ typedef struct menuItem {
   menuItem *children[10];
 };
 
-
+struct menuItem topLevel;
 menuItem *currentMenuItem;
+menuItem *previousMenuItem;
 int currentMenuSelectionIdx;
 
-void drawScreen() {
-  myGLCD.clrScr();
-
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.fillRect(0, 0, 479, 20);
-
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.setBackColor(255, 0, 0);
-
-  myGLCD.setTextSize(100);
-
-  myGLCD.print(currentMenuItem->caption, CENTER, 3);
-  myGLCD.setBackColor(0, 0, 255);
-
+void getChildMenuCount(int* count) {
   for (int i=0; i < 10; i++) {
-    if (currentMenuItem->children[i] != NULL) {
-      currentMenuItem->children[i]->function();
-      myGLCD.print(currentMenuItem->children[i]->caption, LEFT, 30*(i+1));
-    }
+    if (currentMenuItem->children[i] == NULL) break;
+    *count = i+1;
+  } 
+}
+
+void getTextDimensions(String text, uint16_t* w, uint16_t* h) {
+  int16_t  x1, y1;
+  tft.getTextBounds(text, 0, 0, &x1, &y1, w, h);
+}
+
+void dummyHandler(){};
+
+void drawScreen(bool drawTitle) {
+
+  uint16_t text_w, text_h;
+  uint16_t title_w, title_h;
+
+  tft.setTextSize(2);
+  getTextDimensions("AAAA", &text_w, &text_h);
+
+  tft.setTextSize(3);
+  getTextDimensions(currentMenuItem->caption, &title_w, &title_h);
+
+  int width = tft.width();
+  int height = tft.height();
+
+  // tft.drawRect(0, 0, width, height, WHITE);
+  tft.drawRect(SCREEN_MARGIN, SCREEN_MARGIN, width - (SCREEN_MARGIN * 2), height - (SCREEN_MARGIN * 2), WHITE);
+  
+  if (drawTitle) {
+    tft.setTextSize(3);
+    tft.setTextColor(WHITE);
+
+    uint16_t offset = (width / 2) - (title_w / 2);
+    tft.setCursor(offset, 5);
+    tft.setTextColor(BLUE);
+    tft.print(previousMenuItem->caption);
+    tft.setCursor(offset, 5);
+    tft.setTextColor(WHITE);
+    tft.print(currentMenuItem->caption);
   }
 
+  tft.setTextSize(2);
   
-  // delay(500);
+  for (int i=0; i < 10; i++) {
+    if (currentMenuItem->children[i] != NULL) {
+      tft.fillRect(SCREEN_MARGIN + 1, SCREEN_MARGIN + ((text_h + VERTICAL_TEXT_SPACING) * i) + 1, 
+        width - (SCREEN_MARGIN * 2) - 2, text_h + VERTICAL_TEXT_SPACING - 2, BLUE);
+      if (i == currentMenuSelectionIdx) {
+        tft.fillRect(SCREEN_MARGIN + 1, SCREEN_MARGIN + ((text_h + VERTICAL_TEXT_SPACING) * i) + 1, 
+          width - (SCREEN_MARGIN * 2) - 2, text_h + VERTICAL_TEXT_SPACING - 2, WHITE);
+        tft.setTextColor(BLACK);
+      } else {
+        tft.setTextColor(WHITE);
+      }
+      tft.setCursor(TEXT_SCREEN_MARGIN, TEXT_SCREEN_MARGIN + ((text_h + VERTICAL_TEXT_SPACING) * i));
+      tft.print(currentMenuItem->children[i]->caption);
+    }
+  }
+}
+
+void onEb1Clicked(EncoderButton& eb) {
+  Serial.print("eb1 clickCount: ");
+  Serial.println(eb.clickCount());
+}
+
+void onEb1Encoder(EncoderButton& eb) {
+  int oldIndex = currentMenuSelectionIdx;
+  int childCount;
+  int inc = eb.increment();
+  
+  getChildMenuCount(&childCount);
+  Serial.print("Child count:");
+  Serial.println(childCount);
+  
+  currentMenuSelectionIdx = currentMenuSelectionIdx + inc; 
+  
+  if (currentMenuSelectionIdx < 0) currentMenuSelectionIdx = 0;
+  if (currentMenuSelectionIdx >= childCount-1) currentMenuSelectionIdx = childCount-1;
+
+  Serial.print("INC:");
+  Serial.println(eb.increment());
+
+  Serial.print("INDEX:");
+  Serial.println(currentMenuSelectionIdx);
+
+  if (currentMenuSelectionIdx != oldIndex) {
+    drawScreen(false);
+  }  
+  // Serial.println("************");
+  // Serial.print("eb1 incremented by: ");
+  // Serial.println(eb.increment());
+  // Serial.print("eb1 position is: ");
+  // Serial.println(eb.position());
 }
 
 void setup() {
+  pinMode(48, INPUT_PULLUP);
+  pinMode(50, INPUT_PULLUP);
+  uint16_t ID = tft.readID();
 
-  randomSeed(analogRead(0));
+  tft.begin(ID);
+  tft.setRotation(1);
+
+  int width = tft.width();
+  int height = tft.height();
+  tft.fillScreen(BLUE);
   
-  // Setup the LCD
-  myGLCD.InitLCD();
-  myGLCD.setFont(BigFont);
-
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.print("hello...\n");
 
-  struct menuItem topLevel = {"*** Box Jointer ***", testFunction, {
-    new menuItem{"hello2", testFunction2},
-    new menuItem{"hello3", testFunction},
-    new menuItem{"hello4", testFunction}    
+  topLevel = {"*** Box Jointer ***", testFunction, {
+    new menuItem{"Settings", testFunction2},
+    new menuItem{"Programs", testFunction},
+    new menuItem{"Control", testFunction},
+    new menuItem{"Run", testFunction},
   }};
   
   currentMenuItem = &topLevel;
+  previousMenuItem = &topLevel;
   currentMenuSelectionIdx = 0;
 
-  drawScreen();
+  drawScreen(true);
 
   //Link the event(s) to your function
   eb1.setClickHandler(onEb1Clicked);
   eb1.setEncoderHandler(onEb1Encoder);
 }
 
-
-
 void loop() {
-  // drawScreen();
   eb1.update();
+  delay(10);
 }
